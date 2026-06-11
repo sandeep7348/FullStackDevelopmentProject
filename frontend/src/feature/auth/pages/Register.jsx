40eca7ebe858d12e85df862d6447081b7fac1d3a
sand7348 <bharnisandeep63@gmail.com>
2026-06-11 19:42:55 +0530

    message

--- frontend/src/feature/auth/pages/Register.jsx
+++ frontend/src/feature/auth/pages/Register.jsx
@@ -24,7 +24,7 @@ export const Register = () => {
 
       const response = await axios.post(
         "http://localhost:3000/api/register",
-        userData,{withCredentials:true}
+        userData
       );
 
       console.log(response.data);
---
+++ frontend/src/feature/auth/services/auth.api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+//api layer for backend communication what error has come and ui layer to show error
+export async function register(username, email, password) {
+    try{
+       const response = await axios.post("http://localhost:3000/api/register",{
+           username,email,password
+       })
+       return response.data;
+    }
+    catch(error){
+        throw error;
+    }
+}
+
+export async function login(email,password){
+    try{
+      const response=await axios.post("http://localhost:3000/api/login",{email,password})
+      return response.data;
+    }
+    catch(error){
+        throw error;
+    }
+}
