f08262ebf6f8e88ddc44cb51cd9e16f58c5eeae5
sand7348 <bharnisandeep63@gmail.com>
2026-06-12 08:43:57 +0530

    added home

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home=()=>{
+    return(
+        <div className="container">
+            <h1>Welcome to the Home Page</h1>
+        </div>
+    )
+}
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Login } from "./feature/auth/pages/Login";
 import { Register } from "./feature/auth/pages/Register";
-
+import { Home } from "./Home";
 export const routes = createBrowserRouter([
   {
     path: "/login",
@@ -11,4 +11,9 @@ export const routes = createBrowserRouter([
     path: "/register",
     element: <Register />,
   },
+  {
+    path:"/",
+    element:<Home/>
+
+  }
 ]);
